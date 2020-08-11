d8e7844a1304e2a798e48b3558469971993f9500
Rowan Rhodes <rowandrhodes@gmail.com>
2020-08-11 20:32:06 +0100

    .gitignore working now

--- Pods/Target Support Files/Pods-ViperChitChat-ViperChitChatUITests/Pods-ViperChitChat-ViperChitChatUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ViperChitChat_ViperChitChatUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ViperChitChat_ViperChitChatUITests
-@end
